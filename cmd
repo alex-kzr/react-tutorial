dc0402e1cf252019b5b115d014ae4c8df94d9a6e
Alex-SK <aleksey.kozyura@gmail.com>
2020-04-18 19:25:37 +1000

    Add component

--- cmd
+++ cmd
@@ -1,5 +1,6 @@
 npm init
 npm i react react-dom --save
+npm i create-react-class --save-dev
 npm i @babel/core babel-loader @babel/preset-env @babel/preset-react --save-dev
 npm i webpack webpack-cli webpack-dev-server --save-dev
 
--- src/app/index.js
+++ src/app/index.js
@@ -1,1 +1,16 @@
-alert('it wooorks!');
+var React = require('react');
+var ReactDOM = require('react-dom');
+var createReactClass = require('create-react-class');
+
+// create component
+// class TodoComponent extends React.Component{ // without create-react-class lib
+var TodoComponent = createReactClass({
+    render: function(){
+        return(
+            <h1>Hellooooo</h1>
+        );
+    }
+});
+
+// put component into html page
+ReactDOM.render(<TodoComponent />, document.getElementById('todo-wrapper'));
